f400736afad2792eb5a7e9d27b321a9fc6a25705
Carlos <carlosrizzetti@gmail.com>
2026-01-21 16:16:04 -0300

    inicio

--- js/hojaDeImpresion.js
+++ js/hojaDeImpresion.js
@@ -3,15 +3,19 @@ import { MasaDeChocolate } from "../JSON/MASA_DE_CHOCOLATE.js";
 import { recetas } from "../JSON/CUADRADITOS.js";
 import { header, hojaImpresionContainer, inputCantidadMasa, contenedorMain } from "./CONST.js";
 import { mostrarRecetaDeMasa } from "./FUCIONES.js";
+import { budines } from "../JSON/BUDINES.js";
 
 
- let cantidadCuadraditos;
+    let cantidadCuadraditos;
 
 document.addEventListener("DOMContentLoaded", () => {    
     const params = new URLSearchParams(window.location.search);
     const sector = params.get("sector");   
-    header.innerHTML = `<section id="titulo-receta">Receta de ${sector}</section    > 
+    header.innerHTML = `<section id="titulo-receta">Receta de ${sector}</section>
+    <button id="btn-reinicio">Reiniciar</button> 
     <button onclick="window.print()" id="btnImprimir">Imprimir</button> `;
+    const btnReiniciar= document.querySelectorAll("button")
+    console.log(btnReiniciar);
 
      if (sector === "Masa") {
         Masa.forEach(ingrediente => {
@@ -62,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     const recetaElegida = recetas.find(receta => receta.mercaderia === e.target.id);
                     hojaImpresionContainer.innerHTML += `  
                                 <section class="descripcion-producto-item">
-                                  <h3>RELLENO PARA CUADRADITOS DE&nbsp ${e.target.id} </h3> &nbsp    <p> ${e.target.value}&nbspUnid </p>
+                                  <h3>RELLENO PARA CUADRADITOS DE ${e.target.id} </h3><p> ${e.target.value}Unid </p>
                                 </section> `;
                     recetaElegida.ingredientes.forEach(ingrediente => {
                         let ing = ingrediente.cantidad;
@@ -79,4 +83,62 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
+      if (sector === "Budines") {
+        contenedorMain.innerHTML = '<section id="recetas-lista"></section>';
+        const listaRecetas = document.getElementById("recetas-lista");
+        budines.forEach(receta => {
+            listaRecetas.innerHTML += `
+            <section class="receta-budin-item">
+                <div class="btn-receta-item">${receta.mercaderia}</div>
+                <input type="number" id="${receta.mercaderia}" placeholder="Cantidad ">
+            </section>
+                `;
+        });
+        contenedorMain.appendChild(hojaImpresionContainer);
+        cantidadCuadraditos = document.querySelectorAll("input[type='number']");       
+        hojaImpresionContainer.innerHTML = '';
+        cantidadCuadraditos.forEach(input => {
+            input.addEventListener("change", (e) => {
+                e.preventDefault();                
+                if (e.target.value === '') {
+                    hojaImpresionContainer.innerHTML = '';
+                    return;
+                } if (e.target.value > 0) {
+                    const cantidad = e.target.value;
+                    const recetaElegida = budines.find(receta => receta.mercaderia === e.target.id);
+                    hojaImpresionContainer.innerHTML += `  
+                                <section class="descripcion-producto-item">
+                                  <h3>RECETA DE BUDINES ${e.target.id} </h3> <p> ${e.target.value}Budines </p>
+                                </section> `;
+                    recetaElegida.ingredientes.forEach(ingrediente => {
+                        let ing = ingrediente.cantidad;
+                        let total = Number(ing) * Number(cantidad);
+                        hojaImpresionContainer.innerHTML += `  
+                                                       
+                                <section class="receta-item">
+                                   <label>${ingrediente.nombre}</label> 
+                                   <p>${total.toFixed(2)}</p>
+                                 </section>                
+                                   `;
+                    });
+                }
+            });
+        });
+    }
+    btnReiniciar.forEach(element => {    
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(element.id==="btn-reinicio"){
+            hojaImpresionContainer.innerHTML = '';
+            limpiarinput(cantidadCuadraditos);
+        }
+        
+    });
+    });
 });
+function limpiarinput(inputs){   
+    inputs.forEach(input => {
+        input.value='';
+    });
+
+}
