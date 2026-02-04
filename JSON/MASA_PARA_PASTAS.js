d8235a3ca7f66a15eb5ebb2f96844c7d026d8592
Pablo Irusta <pirusta@fsa.com>
2026-02-04 14:08:59 -0300

    ravioles 4/2

---
+++ JSON/MASA_PARA_PASTAS.js
@@ -0,0 +1,23 @@
+export const MASAS= [
+    {
+        mercaderia: "Masa para pascualina",
+        ingredientes: [
+            { nombre: "Harina 0000", cantidad: 0.660 },
+            { nombre: "Manteca", cantidad: 0.105 },
+            { nombre: "Agua", cantidad: 0.191 },
+            { nombre: "Sal fina", cantidad: 0.016 },
+            { nombre: "Conservantes(Propianato + Sorbato)(80Gr en 20Kg de masa)", cantidad: 0.4 }
+        ]
+    },
+    {
+        mercaderia: "Masa para Ravioles(Blanca) ",
+        ingredientes: [
+            { nombre: "Semolin", cantidad: 0.72536 },
+            { nombre: "Manteca", cantidad: 0.01447 },
+            { nombre: "Huevo", cantidad: 0.78121 },
+            { nombre: "Agua", cantidad: 0.20754 },
+            { nombre: "Sal fina", cantidad: 0.00894 },
+            { nombre: "Conservantes(Propianato + Sorbato)(80Gr en 20Kg de masa)", cantidad: 0.38 }
+        ]
+    }
+];
---
+++ JSON/RELLENO_PARA_PASTAS.js
@@ -0,0 +1,14 @@
+export const rellenosParaPastas = [
+    {
+        mercaderia: "Relleno para ravioles de ricota",   
+        ingredientes: [
+            { nombre: "Ricota", cantidad: 0.9313 },
+            { nombre: "Queso rallado", cantidad: 0.0388 },
+            { nombre: "Sal fina", cantidad: 0.0075 },
+            { nombre: "Nuez moscada", cantidad: 0.313 },
+            { nombre: "Pimienta blanca", cantidad: 0.313 },
+            { nombre: "Galleta molida", cantidad: 0.050 },
+            { nombre: "Conservante (Benzoato+Sorbato) 1g x kg relleno", cantidad: 0.160 }
+        ]
+    }
+];
